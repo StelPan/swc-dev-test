99cea3635973c9ef44db53dc49cdc4c7389631aa
Stepan Gubin <s.gubin@fterra.ru>
2023-06-23 19:17:33 +0700

    Вынес компоненты отображения событий в layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,33 +10,6 @@
             </div>
         </div>
         <div class="row">
-            <div class="col-lg-4">
-                <h2>Все события</h2>
-                <a href="{{route('events.create')}}">Создать событие</a>
-
-                <div class="card mb-2">
-                    <div class="card-header">Все события</div>
-                    <ul class="list-group list-group-flush">
-                        <event-list-component />
-                    </ul>
-                </div>
-
-                <div class="card">
-                    <div class="card-header">Мои события</div>
-                    <ul class="list-group list-group-flush">
-                        @foreach($events as $event)
-                            @if($event->user->id === $user->id)
-                                <li class="list-group-item">
-                                    <a href="{{ route('events.show', ['event' => $event->id]) }}">
-                                        {{$event->title}}
-                                    </a>
-                                </li>
-                            @endif
-                        @endforeach
-                    </ul>
-                </div>
-            </div>
-            <div class="col-lg-8"></div>
         </div>
     </div>
 @endsection
