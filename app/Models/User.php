fa8294623a4ce0ba97135098560615e070db0855
Stepan Gubin <s.gubin@fterra.ru>
2023-06-23 10:26:04 +0700

    Добавление новых полей: фамилия и дата рождения.

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,8 +19,10 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'surname',
         'email',
         'password',
+        'birthday',
     ];
 
     /**
@@ -41,4 +43,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function events ()
+    {
+        return $this->belongsToMany(Event::class, 'event_user');
+    }
 }
