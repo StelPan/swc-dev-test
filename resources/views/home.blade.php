8b21b1f0f00000d5ff8450ab225b1efe6eb056e8
Stepan Gubin <s.gubin@fterra.ru>
2023-06-23 11:38:09 +0700

    Отображение событий на главной странице

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,48 @@
-@extends('layouts.app')
+@extends('layouts.auth')
+
+@section('title', 'Главная страница')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <h1>Главная страница</h1>
             </div>
         </div>
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>Все события</h2>
+                <a href="{{route('events.create')}}">Создать событие</a>
+
+                <div class="card mb-2">
+                    <div class="card-header">Все события</div>
+                    <ul class="list-group list-group-flush">
+                        @foreach($events as $event)
+                            <li class="list-group-item">
+                                <a href="{{ route('events.show', ['event' => $event->id]) }}">
+                                    {{$event->title}}
+                                </a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+
+                <div class="card">
+                    <div class="card-header">Мои события</div>
+                    <ul class="list-group list-group-flush">
+                        @foreach($events as $event)
+                            @if($event->user->id === $user->id)
+                                <li class="list-group-item">
+                                    <a href="{{ route('events.show', ['event' => $event->id]) }}">
+                                        {{$event->title}}
+                                    </a>
+                                </li>
+                            @endif
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-8"></div>
+        </div>
     </div>
-</div>
 @endsection
