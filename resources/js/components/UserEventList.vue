436428185dc373c9f04ee01e32968f9fdd5b9e89
Stepan Gubin <s.gubin@fterra.ru>
2023-06-23 19:16:36 +0700

    Компонентр отображения событий пользователя

---
+++ resources/js/components/UserEventList.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item" v-for="(event) in events" :key="event.id">
+                <a :href="`/events/${event.id}`">
+                    {{event.title}}
+                </a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import {loadUserEvents} from '../services/event';
+
+export default {
+    data() {
+        return {
+            events: [],
+            timer: null,
+        }
+    },
+    methods: {
+        async loadEvents () {
+            try {
+                this.events = await loadUserEvents(this.$props.userId);
+            } catch (error) {
+                console.log(error);
+            }
+        },
+        async aggregateEvents () {
+            this.timer = setInterval(this.loadEvents, 1000 * 30);
+        }
+    },
+    props: {
+        userId: {
+            required: true,
+            type: Number,
+        }
+    },
+    async mounted() {
+        await this.loadEvents();
+        await this.aggregateEvents();
+    },
+}
+</script>
