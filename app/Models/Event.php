346395e97b2da9170e502c2c69593682a75fad87
Stepan Gubin <s.gubin@fterra.ru>
2023-06-23 10:25:35 +0700

    Модель таблицы события

---
+++ app/Models/Event.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+    ];
+
+    /**
+     * The attributes that are mass assignable.
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'event_user');
+    }
+}
