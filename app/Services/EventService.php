85a6e7effd56047779530718a85dddd356568af3
Stepan Gubin <s.gubin@fterra.ru>
2023-06-23 13:45:20 +0700

    Добавление методов: поиск события и присоединения пользователя к событию

--- app/Services/EventService.php
+++ app/Services/EventService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Models\Event;
+use App\Models\User;
 
 class EventService
 {
@@ -23,6 +24,16 @@ class EventService
     }
 
     /**
+     * Get event by id.
+     * @param $id
+     * @return \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Eloquent\Builder[]|\Illuminate\Database\Eloquent\Collection|\Illuminate\Database\Eloquent\Model|null
+     */
+    public function getEventById($id)
+    {
+        return Event::with('user', 'users')->find($id);
+    }
+
+    /**
      * Create a new event
      * @param array $data
      * @return mixed
@@ -30,5 +41,17 @@ class EventService
     public function createEvent(array $data)
     {
         return $this->model->create($data);
+    }
+
+    /**
+     * @param int $id
+     * @param int $user_id
+     * @return mixed
+     */
+    public function touchEvent(int $id, int $user_id)
+    {
+        $event = Event::find($id);
+        $event->users()->attach(User::find($user_id));
+        return $event;
     }
 }
