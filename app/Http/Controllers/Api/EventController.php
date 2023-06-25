958de4200c13f427d27d61124fa821731d652b42
SteLpan <20step01@gmail.com>
2023-06-25 14:21:30 +0700

    Добавление метода создания события в апи контроллер

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Services\EventService;
+use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
@@ -40,9 +41,26 @@ class EventController extends Controller
         return response()->json(['event' => $event]);
     }
 
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function create(Request $request)
+    {
+        $event = $this->eventService->createEvent(array_merge($request->all(), ['user_id' => auth()->user()->id]));
+        return response()->json(['message' => 'Event create successfully', 'event' => $event]);
+    }
+
+    public function destroy()
+    {
+        // TODO: Destroy event
+
+    }
+
     public function join($id)
     {
         $this->eventService->touchEvent($id, Auth::user()->id);
         return response()->json(['success' => true, 'message' => 'Event joined successfully']);
     }
 }
+                        
